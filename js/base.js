ec605a35e58a9fc23730ce2f857bb530c57132a9
xiexiaotian <xiexiaotian9527@163.com>
2018-02-14 13:24:25 +0800

    主页完成

---
+++ js/base.js
@@ -0,0 +1,29 @@
+$(
+    function () {
+        // 启用全屏滚动插件
+        $('#fullpage').fullpage({
+            navigation: true,
+            navigationTooltips: ['首页', '关于我', '技能掌握', '作品Demo', '联系我']
+        });
+
+        // ---首页背景轮播效果---
+        var home_bgs = $('.home-bg'),
+            bg_index = 1;
+        //  设置切换背景图的定时器
+        var bg_intervar = setInterval(
+            function () {
+                // 给下个背景图添加显示的Class,并清除其余背景图现实的Class
+                home_bgs.eq(bg_index)
+                    .addClass('active')
+                    .siblings()
+                    .removeClass('active');
+                // 更新该显示的下张图片索引
+                bg_index < home_bgs.length - 1 ? bg_index++ : bg_index = 0;
+            },
+            5000
+        )
+
+
+
+    }
+);
